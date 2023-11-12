0447e3c180db7672ff1f7c0251c7cfd2972f353f
Somkesharwani <kesharwanisom14@gmail.com>
2023-11-12 13:49:27 +0000

    latest repo

--- Product/models.py
+++ Product/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.conf import settings
+import uuid
 
 
+
+def setDefault():
+    return ProductCategory.objects.get_or_create(name='default', type="Generic")
 
 def get_sentinel_category():
     return ProductCategory().objects.get_or_create(name="Default",parentCategory="Default")[0]
@@ -12,6 +16,15 @@ class ProductCategory(models.Model):
     def __str__(self) -> str:
         return self.name
     choices =  (('Generic', 'Generic'), ('Specific', 'Specific'))
+    level = models.IntegerField(default=1)
     name = models.CharField(max_length=50)
-    type = models.CharField(max_length=15,choices=choices,blank=False,null=False,default=3)
-    parentCategory = models.ForeignKey(to = "ProductCategory",on_delete=models.SET(get_sentinel_category),null=True)
+    type = models.CharField(max_length=15,choices=choices,blank=False,null=False,default='Generic')
+    parentCategory = models.ForeignKey(to = "ProductCategory",on_delete=models.SET_DEFAULT, default=setDefault)
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    SKU = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
+    created_at = models.DateTimeField(auto_now=True,)
+    modified_st = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey("ProductCategory",on_delete=models.SET_DEFAULT, default=setDefault)
--- Product/serializers.py
+++ Product/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from .models import ProductCategory
+from .models import ProductCategory, Product
 
 class UserSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -22,12 +22,25 @@ class CategorySerializer(serializers.HyperlinkedModelSerializer):
         parentCategory = validated_data.get("parentCategory", None)
         validated_data.pop("parentCategory")
         print("Datav 121", parentCategory)
-        if parentCategory == None or parentCategory == "Default":
-            cat = ProductCategory.objects.get_or_create(name = "Default",type="Generic")[0]
+        if parentCategory == None or parentCategory == "default":
+            cat = ProductCategory.objects.get_or_create(name = "default",type="Generic")[0]
         else:
             cat = ProductCategory.objects.filter(name=parentCategory)[0]
 
         #prod = ProductCategory.objects.get_or_create(name, type, obj)
         return ProductCategory.objects.create(parentCategory=cat, **validated_data)
+   
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+        subCategory = serializers.CharField(max_length=10)
+        class Meta:
+            model = Product
+            fields = ('name','description', 'subCategory')
+        
+        def create(self, validated_data):
+            sub = validated_data.get("subCategory")
+            sub= ProductCategory.objects.get(name=sub)
+            validated_data.pop("subCategory")
+            level = 1
+            return Product.objects.create(subCategory=sub, **validated_data)
    
 
